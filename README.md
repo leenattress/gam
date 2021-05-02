fc6f33014e4d8b826fce0183a1d6eaad8c36930a
Lee Nattress <leenattress1981@gmail.com>
2021-05-02 20:43:08 +0100

    mvp in readme and joypad shooty example

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# Webpack Frontend Starterkit
+# DIY Minimalist WebGL Game Engine
 
-[![Dependabot badge](https://flat.badgen.net/dependabot/wbkd/webpack-starter?icon=dependabot)](https://dependabot.com/)
-
-A lightweight foundation for your next webpack based frontend project.
+A javascript game engine with simplicity in mind. Only extremely basic features are present.
 
 ### Installation
 
@@ -24,10 +22,46 @@ npm run build
 
 ### Features:
 
+- WebGl canvas for accellerated gameplay
+- SHAPES! - rectangles now available!
 - ES6 Support via [babel](https://babeljs.io/) (v7)
 - JavaScript Linting via [eslint-loader](https://github.com/MoOx/eslint-loader)
-- SASS Support via [sass-loader](https://github.com/jtangelder/sass-loader)
+- SASS Support (for area around the game canvas) via [sass-loader](https://github.com/jtangelder/sass-loader)
 - Autoprefixing of browserspecific CSS rules via [postcss](https://postcss.org/) and [autoprefixer](https://github.com/postcss/autoprefixer)
 - Style Linting via [stylelint](https://stylelint.io/)
 
 When you run `npm run build` we use the [mini-css-extract-plugin](https://github.com/webpack-contrib/mini-css-extract-plugin) to move the css to a separate file. The css file gets included in the head of the `index.html`.
+
+### Roadmap
+
+- Graphics
+  - Images
+    - 🔲 Scale
+    - 🔲 Rotate
+    - 🔲 Transparency
+    - 🔲 Skew
+  - Simple primitives
+    - ✅ Rectangle
+    - 🔲 Line
+    - 🔲 Circle
+  - Text
+    - 🔲 Font
+    - 🔲 Size
+    - 🔲 Color
+- Sound
+  - 🔲 Samples
+  - 🔲 Looping
+  - 🔲 Volume
+- Input
+  - ✅ Mouse
+  - 🔲 Keyboard
+  - 🔲 Joypad
+  - 🔲 Mobile
+- Helpers
+  - 🔲 Collisions
+  - Behaviours
+    - 🔲 Top down
+    - 🔲 Platformer
+    - 🔲 Shoot-em-up
+    - 🔲 etc...
+
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,67 @@
 import "../styles/index.scss";
-import { Stage, Cast, Utils } from './game';
+import { Stage, Cast, Utils, GamePad } from './game';
 import { easeOutCubic } from './tween';
 
 if (process.env.NODE_ENV === "development") {
   require("../index.html");
 }
 
+const cast = new Cast();
+
 const config = {
   width: 800,
   height: 480
 };
 
-const cast = new Cast();
+const gamepad = new Gamepad();
+let joyPadFollower;
+gamepad.on('connect', e => {
+  console.log(`controller ${e.index} connected!`);
+  joyPadFollower = cast.addActor({
+    onInit: function () {
+      this.x = 300;
+      this.y = 300;
+    },
+    onUpdate: function () {
+      this.move();
+      let rectangleColor = [1, 1, 1, 1];
+      stage.drawRectFill(rectangleColor, this.x - 15, this.y - 15, 60, 30);
+    }
+  });
+});
+gamepad.on('disconnect', e => {
+  console.log(`controller ${e.index} disconnected!`);
+  if (joyPadFollower) {
+    cast.destroyActorById(joyPadFollower.key);
+    joyPadFollower = null;
+  };
+});
+gamepad.on('press', 'button_1', e => {
+  if (joyPadFollower) {
+    const thisActor = cast.getActor(joyPadFollower);
+    let i;
+    for (i = 0; i < (Utils.getRandomInt(5) + 5); i++) {
+      makeShootyBullet(thisActor.x, thisActor.y);
+    };
+  }  
+
+});
+gamepad.on('hold', 'stick_axis_left', e => {
+  if (joyPadFollower) {
+    const thisActor = cast.getActor(joyPadFollower);
+    thisActor.vx = e.value[0] * 5;
+    thisActor.vy = e.value[1] * 5;
+  }
+});
+gamepad.on('release', 'stick_axis_left', e => {
+  if (joyPadFollower) {
+    const thisActor = cast.getActor(joyPadFollower);
+    thisActor.vx = 0;
+    thisActor.vy = 0;
+  }
+});
+
+
 
 const stage = new Stage({
   target: 'canvas',
@@ -47,6 +97,28 @@ const mouseFollower = cast.addActor({
     //stage.drawRectFill(rectangleColor, 200, 200, 80, 80);
   }
 });
+
+
+
+const makeShootyBullet = (x, y) => {
+  const actorId = cast.addActor({
+    onInit: function () {
+      this.x = x + 40;
+      this.y = y;
+      this.vx = 10;
+    },
+    onUpdate: function () {
+      this.tick();
+      this.move();
+      if (this.time > 240) { cast.destroyActorById(this.key); }
+      let rectangleColor = [1, 0, 1, 1];
+      let size = 10;
+      let half = size / 2;
+      stage.drawRectFill(rectangleColor, this.x - half, this.y - half, size, size);
+    }
+  });
+};
+
 
 const makeSmallParticle = (x, y) => {
   const actorId = cast.addActor({
